c372f3cf191531b31fde95bb44c37554700b3de7
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-19 23:08:55 -0500

    Done

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,12 @@
+require 'pry'
 # Write your code here
+
+def reverse_each_word(sentence)
+  sentence_arr = sentence.split
+  reversedArr = sentence_arr.map(&:reverse)
+  reversedArr.join(" ")
+  # sentence.split.map(&:reverse).join(" ")
+end
+
+binding.pry
+0
